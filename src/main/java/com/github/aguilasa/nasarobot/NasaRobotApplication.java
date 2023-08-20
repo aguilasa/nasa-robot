fd35ee00288ca913c59baeaf486f7b0365773975
Ingmar Schmidt de Aguiar <ingmar.aguiar@gmail.com>
2023-08-20 14:38:49 -0300

    Cria projeto Spring Boot inicial

---
+++ src/main/java/com/github/aguilasa/nasarobot/NasaRobotApplication.java
@@ -0,0 +1,13 @@
+package com.github.aguilasa.nasarobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaRobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/aguilasa/nasarobot/NasaRobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aguilasa.nasarobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaRobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
