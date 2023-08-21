0940597b8377fe2db5c224d2b45883e2de686594
Ingmar Schmidt de Aguiar <ingmar.aguiar@gmail.com>
2023-08-21 20:53:27 -0300

    Ajusta a enumeração Orientation para usar o tamanho ao invés de um número fixo

--- src/main/java/com/github/aguilasa/nasarobot/dto/Orientation.java
+++ src/main/java/com/github/aguilasa/nasarobot/dto/Orientation.java
@@ -3,11 +3,13 @@ package com.github.aguilasa.nasarobot.dto;
 public enum Orientation {
     N, E, S, W;
 
+    private static final int SIZE = values().length;
+
     public Orientation rotateLeft() {
-        return values()[(ordinal() + 3) % 4];
+        return values()[(ordinal() + (SIZE - 1)) % SIZE];
     }
 
     public Orientation rotateRight() {
-        return values()[(ordinal() + 1) % 4];
+        return values()[(ordinal() + 1) % SIZE];
     }
 }
